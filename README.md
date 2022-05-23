92d0c84c1e71aaa0729c95a70a5adf4e01120ac0
Quentin ROSA <quentin.rosa@technicolor.com>
2022-05-23 12:21:50 +0200

    feat: loadfile finished
    - Get file from S3
    - Add Spinner for the long phase
    - Init SQL Db
    - Add type on column
    - Simple Request
    - Error management
    - Typing variables
    - Bool option [-d, -c]
    - Avoid insert same file multiple times
    - Remove duplicated data and save the total count instead
    - Set the previous month by default, so can call the script once a month the 1st by cron job
    - Add message for the prompt option

--- README.md
+++ README.md
@@ -1,1 +1,183 @@
-# Madkudu
+# **Madkudu**
+
+Hello, welcome on the madkudu exercice.
+
+---
+## **Installation**
+
+```
+pip install -r requirement.txt
+./main.py --help
+```
+
+---
+## **Usage**
+
+You will find 3 commands:
+
+- **loadfile**      -- Download file and load it in the sqlite DB
+- **downloadfile**  -- Just download the target file in local
+- **testdb**        -- Check if the given database exist
+
+
+#### -- **Loadfile** --
+The main function `loadfile` will offer you a way to download a file from any s3 host.
+
+By default, the downloaded file will be `s3://work-sample-mk/YEAR/MONTH/events.csv`
+
+**YEAR** and **MONTH** are the values of the current date **minus 1 month**.
+
+This will let you use the script to automate the download of file each 1st of month, by a `conjob` or in `Airflow` for exemple.
+
+A feature will block a file if this one has already been inserted in the DB.
+
+
+
+`main.py loadfile [OPTIONS]`
+
+By default:
+```
+./main.py loadfile
+```
+You will get a *Madkudu_events.db* in your current folder
+
+
+
+For futur evolutions, you can also pass those options:
+**-y, --year** TEXT      Format YYYY::2022
+**-m, --month** TEXT     Format MM::04
+Will let you choose a specific date
+```
+./main.py loadfile -y2021 -m04
+```
+
+**-h, --host** TEXT      S3 path::s3://work-sample-mk
+Will let you choose a specific s3 host
+
+**-f, --filename** TEXT  File name::events.csv
+Will let you choose a specific file name
+
+**--dbname** TEXT        Chose the destination::Madkudu_events.db
+Will let you specify the path and the name of you destination DB
+
+
+The exercice was to get CSV data from an S3 host and provide the data in a usable format to be able to make request.
+In this purpose, you would like to clean the data:
+**-c, --clean**          Clean the data
+
+```
+./main.py loadfile -y2021 -m04 --clean
+```
+
+Currently, this option will merge duplicated rows and add a counter in a new column.
+In addition, we could imagine to remove data outside de current month.
+
+**-d, --debug**          Show error details
+```
+./main.py loadfile -y1850 -m04 -d
+```
+Allow the program to return stack trace for debug purpose. Else a simple error message is shown.
+
+
+#### -- **Downloadfile** --
+
+Give you a way to download the file without insert in a sqlite DB
+
+`./main.py downloadfile [OPTION] [DEST] [SOURCE]`
+
+```
+./main.py downloadfile 'madkudu_event.csv' 's3://work-sample-mk/2021/04/events.csv'
+```
+You will get a fresh file `madkudu_event.csv` in your current directory
+
+**DEST** - Specify the destination path and the filename\n
+    Default: './{year}_{month}_{filename}'\n
+**SOURCE** - Specify the source path and the filename\n
+    Default: '{host}/{year}/{month}/{filename}'
+
+
+#### -- **Testdb** --
+Give you a simple way to check if the DB exist and is available
+
+`main.py testdb [OPTIONS] [DBNAME]`
+
+```
+main.py testdb
+```
+
+**DBNAME** - Specify the full path of your targeted DB
+    Default:'Madkudu_events.db'
+
+
+---
+## **Perspectives and evolutions**
+
+So for now, the program is able to download a file from an s3 and to insert it in a sqliteDB
+
+For an industrialisation purpose I would give you some suggestions:
+
+### - On a ops perspective:
+- Use Docker
+- Call the script as purpose each 1st of the month with a cronjob or with Airflow for example
+- Create a way to notice admin in case of fail. This may be done by an email or by using a trigger on the logs (see next point)
+
+### - On a code perspective:
+- Add a log function with level of log
+
+Logs could be formated as followed:
+
+`[Date Hours] [Unique Token] [Level Log] : Message`
+
+Logs should offer the capacity to print the log and to save them in a specific file
+
+We should also be able to set a log level by default to avoid to much verbose logs output
+
+Levels could be (in order of priority): `[ERR_TRACE, WARN_TRACE, ERR, WARN, INFO, DEBUG, TRACE, LOG]`
+
+
+### - On data perspective:
+- Use postgreSQL instead of sqlite as a DB
+- We could add more cleaning transformation.
+
+I would also suggest to decoralate this point of the code and create an external config file
+
+
+## **Development worflow**
+
+I started by working on the file download.
+I looked for a tool to get file on `S3` and so I used the lib `s3fs`
+
+In order to offer a good experience with the tool, I implemented a CLI lib at the first step of the development.
+I found the CLI `Click` which respected some conditions. The documentation is **clear** and **full**. The tool is complete with **lot of options** to customize easily the CLI. The use of the CLI is implemented by **decorator**. So its code is writen **outside the code logic** and keep the code **clean and simple**.
+
+I also add a spinner for the long phases of download or write.
+
+I still needed to offer a way to manage data in a proper form so I used `sqlite3` for its simplicity.
+
+Before the insertion, I use a **file-like** python object : `io.BytesIO()`
+
+This let to not write the file on the file system and to keep it in RAM after the download. I save some compute time but, in the futur, as files will become too big, this `optimisation` should be abandoned.
+
+I also use **pandas** to insert the CSV into the sqlite database.
+
+At this point, the big parts of the exercice have been done.
+
+
+I restarted to work on it 2 days later, to add some options and flexibility.
+
+So, I added a condition to avoid to insert two times the same file. I used a new table in the DB.
+
+I also add Type on column during the table creation.
+
+I added an error management and a debug option to keep the tool clean for a casual usage.
+
+I also finetuned all the options of the CLI to respect the consign to have a tool `fully automated and able to recurrently
+and periodically retrieving and processing the data without any external action`
+That why, by default, the tool get the date of the previous month with the aim to run it periodically every 1st of the month.
+
+And to open a way of the data transformation, I add a feature `--clean` to merge duplicated rows and add a column 'count' to keep the information.
+In addition of this, we could imagine to remove rows outside the scope of the targeted month or to add validation on the data (email format, enum on `action` and `uri` columns, ...)
+
+
+I hope you will enjoy my work and call me back for the next step.
+Thank you
--- main.py
+++ main.py
@@ -5,80 +5,166 @@ import s3fs
 import click
 from halo import Halo
 
-import sqlite3
 import pandas
+import sqlite3
+import sys
+import traceback
+
+from datetime import datetime
+import dateutil.relativedelta
+
+current_time = datetime.now() - dateutil.relativedelta.relativedelta(months=1)
+
+# import pdb
+# pdb.set_trace()
 
 
 @click.group()
 def main():
     """
-    CLI to download CSV files from s3 and load them in an sqliteDB
+    CLI to download CSV files from s3 and load them in an sqlite DB
     """
     pass
 
-@main.command()
-@click.option('--year', default="2021", help="Format YYYY")
-@click.option('--month', default="04", help="Format MM")
-@click.option('--host', default="s3://work-sample-mk")
-@click.option('--filename', default="events.csv")
-def getFile(year, month, host, filename):
-    print(f"Args == {year}, {month}, {host}, {filename}") # DEBUG
-
-    fs = s3fs.S3FileSystem(anon=True)
-
-    with fs.open(f"{host}/{year}/{month}/{filename}", 'rb') as file:
-        out = open(f"{year}_{month}.csv", 'wb')
-        with Halo(text=f"Writing '{year}_{month}.csv' in progress", spinner='dots'):
-            out.write(file.read())
-        print("'{year}_{month}.csv' has been written")
-
 
 @main.command()
-@click.option('--dbname', default="Madkudu_events.db", help="Check if the DB Exist")
-def testDB():
+@click.option('--year', '-y', prompt='Select the year', prompt_required=False, default=f"{current_time.year}", help=f"Format YYYY::{current_time.year}")
+@click.option('--month', '-m', prompt='Select the month', prompt_required=False, default=f"{current_time.month:02}", help=f"Format MM::{current_time.month:02}")
+@click.option('--host', '-h', default="s3://work-sample-mk", help="S3 path::s3://work-sample-mk")
+@click.option('--filename', '-f', default="events.csv", help="File name::events.csv")
+@click.option('--dbname', default="Madkudu_events.db", help="Chose the destination DB::Madkudu_events.db")
+@click.option('--clean', '-c', is_flag=True, help="Clean the data")
+@click.option('--debug', '-d', is_flag=True, help="Show error details")
+def loadFile(year: str, month: str, host: str, filename: str, dbname: str, clean :bool, debug :bool):
+    """Download file and load it in the sqlite DB\n
+    --year, --month can be passed without value. You will get a prompt which invit you to pass your value.
+    If these options are not passed, the default value will be the last month of the current year.\n
+    You can decide to clean the data thanks to the --clean option\n
+    This will aggregate the duplicated rows and add a column 'count' with the total count for each rows"""
 
-    conn = sqlite3.connect('Madkudu_events.db')
-    cursor = conn.cursor()
-
-    res = cursor.execute("SELECT sqlite_version();").fetchone()
-    print(f"Version=={res[0]}")
-
-
-@main.command()
-@click.option('--year', default="2021", help="Format YYYY")
-@click.option('--month', default="04", help="Format MM")
-@click.option('--host', default="s3://work-sample-mk")
-@click.option('--filename', default="events.csv")
-def loadFile(year, month, host, filename):
-
-    # Create S3 Object to connect without credential to the S3 Bucket
-    fs = s3fs.S3FileSystem(anon=True)
-
-    filelike = io.BytesIO()
-    with fs.open(f"{host}/{year}/{month}/{filename}", 'rb') as file:
-        with Halo(text=f"Downloading '../{year}/{month}/events.csv' in progress", spinner='dots'):
-            filelike.write(file.read())
-            # Reset the position of the filelike cursor
-            filelike.seek(0)
-        print("File downloaded")
-
-
-        data = pandas.read_csv(filelike)
-        # Data loaded in Pandas format
-
-        conn = sqlite3.connect('Madkudu_events.db')
+    try:
+        # Init connection with the DB and create Tables if not exist yet
+        conn = sqlite3.connect(dbname)
         cursor = conn.cursor()
+        print("Connection to the DB established")
 
-        # Create the table
-        cursor.execute("CREATE TABLE IF NOT EXISTS Events (id,timestamp,email,country,ip,uri,action,tags)")
-        print("Table created")
+        cursor.execute("CREATE TABLE IF NOT EXISTS Events (id TEXT,timestamp DATE, email TEXT, country TEXT,ip TEXT, uri TEXT, action TEXT, tags TEXT, count INT)")
+        cursor.execute("CREATE TABLE IF NOT EXISTS Files (name TEXT PRIMARY KEY NOT NULL)")
+      
+        # Check if the file has already been added in the DB
+        notNewFile = cursor.execute("SELECT name FROM Files WHERE name=?", (f"{host}/{year}/{month}/{filename}",)).fetchone()
+        if notNewFile:
+            print(f"The file '../{year}/{month}/{filename}' has already been inserted in the DB")
+        else:
+            # Create S3 Object to connect anonymously to the S3 Bucket
+            fs = s3fs.S3FileSystem(anon=True)
 
-        with Halo(text=f"Adding '../{year}/{month}/events.csv' to the DB in progress", spinner='dots'):
-            data.to_sql('Events', conn, if_exists='append', index = False)
+            filelike = io.BytesIO()
+            with fs.open(f"{host}/{year}/{month}/{filename}", 'rb') as file:
+                with Halo(text=f"Downloading '../{year}/{month}/{filename}' in progress", spinner='dots'):
+                    filelike.write(file.read())
+                    filelike.seek(0) # Reset the position of the filelike cursor
+                print("File downloaded")
+
+            # Load Data in Pandas format
+            with Halo(text=f"Convert CSV into Pandas format in progress", spinner='dots'):
+                df = pandas.read_csv(filelike)
+            print("Data converted into Pandas format")
+
+            if clean:
+                with Halo(text=f"Cleaning data in progress", spinner='dots'):
+                    # Aggregate duplicated lines and add a new 'count' columns
+                    df = df.groupby(df.columns.tolist()).size().to_frame('count').reset_index()
+                    df = df.drop_duplicates()
+                print(f"Data has been cleaned")
+
+            # Add Data into sqlite DB
+            with Halo(text=f"Adding '../{year}/{month}/{filename}' to the DB in progress", spinner='dots'):
+                df.to_sql('Events', conn, if_exists='append', index=False)
             print(f"'{year}_{month}.csv' has been added to the DB")
 
-        rows = cursor.execute('''SELECT * FROM events''').fetchmany(20)
-        print(f"rows=={rows}")
+            # Insert downloaded file name in the DB
+            cursor.execute("INSERT INTO Files VALUES (?)", (f"{host}/{year}/{month}/{filename}",))
+            print(f"'{host}/{year}/{month}/{filename}' has been added into the 'Files' table")
+            conn.commit() # Need to commit to save the insert in the DB
+            conn.close()
+
+    except PermissionError:
+        eprint(f"Error -- You don't have the required permission to access this file")
+    except Exception:
+        if debug:
+            eprint(f"Error -- Exit the program...\n{traceback.print_exception(*sys.exc_info())}")
+        else:
+            eprint("Error -- Exit the program.")
+
+
+@main.command()
+@click.option('--year', '-y', prompt='Select the year', prompt_required=False, default=f"{current_time.year}", help=f"Format YYYY::{current_time.year}")
+@click.option('--month', '-m', prompt='Select the month', prompt_required=False, default=f"{current_time.month:02}", help=f"Format MM::{current_time.month:02}")
+@click.option('--host', '-h', default="s3://work-sample-mk", help="S3 path::s3://work-sample-mk")
+@click.option('--filename', '-f', default="events.csv", help="File name::events.csv")
+@click.option('--debug', '-d', is_flag=True, help="Show error details")
+@click.argument('dest', default="")
+@click.argument('source', default="")
+def downloadFile(year: str, month: str, host: str, filename: str, source: str, dest: str, debug: bool):
+    """Just download the target file in local\n
+    --year, --month can be passed without value. You will get a prompt which invit you to pass your value.
+    If these options are not passed, the default value will be the last month of the current year.\n
+    DEST - Specify the destination path and the filename\n
+        Default: './{year}_{month}_{filename}'\n
+    SOURCE - Specify the source path and the filename\n
+        Default: '{host}/{year}/{month}/{filename}'"""
+
+
+    # Create S3 Object to connect anonymously to the S3 Bucket
+    fs = s3fs.S3FileSystem(anon=True)
+
+    if not source:
+        source = f"{host}/{year}/{month}/{filename}"
+
+    if not dest:
+        dest = f"./{year}_{month}_{filename}"
+
+    # Download and write the targeted file 
+    try:
+        print(f"Download {source} start")
+        with fs.open(source, 'rb') as file:
+            out = open(f"{dest}", 'wb')
+            with Halo(text=f"Writing '{dest}' in progress", spinner='dots'):
+                out.write(file.read())
+            print(f"'{dest}' has been written")
+    except ValueError:
+        eprint(f"{source} if not a valid source")
+    except Exception:
+        if debug:
+            eprint(f"Error -- Year=={year}, Month=={month}\nExit the program...\n{traceback.print_exception(*sys.exc_info())}")
+        else:
+            eprint(f"Error -- Year=={year}, Month=={month}\nExit the program.")
+
+
+@main.command()
+@click.option('--debug', '-d', default=False, help="Show error details")
+@click.argument('dbname', default="Madkudu_events.db")
+def testDB(dbname: str, debug: bool):
+    """Check if the given database exist\n
+    DBNAME - Specify the full path of your targeted DB\n
+            Default:'Madkudu_events.db'"""
+
+    try:
+        conn = sqlite3.connect(f'file:{dbname}?mode=ro', uri=True)
+        print(f"Success -- Connection to the '{dbname}' database established")
+        conn.close()
+    except sqlite3.OperationalError:
+        eprint(f"Failed -- Fail to connect to the '{dbname}' database")
+    except Exception:
+        if debug:
+            eprint(f"Error -- Exit the program...\n{traceback.print_exception(*sys.exc_info())}")
+        else:
+            eprint("Error -- Exit the program.")
+
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
 if __name__ == "__main__":
     main()
